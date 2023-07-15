ea2d6650019a7df9ca668a0854498c722147e60f
PaulBlanco29 <joevyblanco29@gmail.com>
2023-07-15 04:00:34 -0400

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FiSearch, FiCloud, FiSun, FiCloudRain } from 'react-icons/fi';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const API_KEY = 'fc9fd7319c4c8006642ec34e19a21cf0';
+
+  const searchWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+      );
+      setWeather(response.data);
+      setError(null);
+    } catch (error) {
+      setWeather(null);
+      setError(error.message);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      searchWeather();
+    }
+  };
+
+  const getWeatherIcon = (weather) => {
+    if (weather === 'Clouds') {
+      return <FiCloud className="text-gray-500 text-3xl mt-2" />;
+    } else if (weather === 'Clear') {
+      return <FiSun className="text-yellow-500 text-3xl mt-2" />;
+    } else if (weather === 'Rain') {
+      return <FiCloudRain className="text-blue-500 text-3xl mt-2" />;
+    } else {
+      return <FiSun className="text-gray-500 text-3xl mt-2" />;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="bg-white rounded-lg shadow p-8 max-w-xs w-full">
+        <h1 className="text-2xl font-bold mb-6">Weather App</h1>
+        <div className="flex items-center border border-gray-300 rounded-md p-2">
+          <input
+            type="text"
+            className="flex-1 outline-none text-gray-600"
+            placeholder="Enter a city"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <button className="flex-none text-gray-600" onClick={searchWeather}>
+            <FiSearch />
+          </button>
+        </div>
+        {error && <p className="text-red-500 mt-4">{error}</p>}
+        {weather && (
+          <div className="mt-4">
+            <p className="text-xl">{weather.name}</p>
+            <p className="text-gray-500">{weather.weather[0].description}</p>
+            {getWeatherIcon(weather.weather[0].main)}
+            <p className="text-4xl font-bold mt-2">
+              {Math.round(weather.main.temp - 273.15)}°C
+            </p>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
